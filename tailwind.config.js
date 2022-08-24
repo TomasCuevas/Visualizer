6b08027981a482e31b01c8927428b0530c0fabe7
Tomas Cuevas <memo_mister398@hotmail.com>
2022-08-23 23:37:31 -0300

    chore(Root): changes in .gitignore, tailwind.config.js and add CHANGELOG.md file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#8fccfc",
+        lighttext: "#feffff",
+        darktext: "#181919",
+        darklighttext: "#514f52",
+        decoratelight: "#feffff",
+        decoratedark: "#181919",
+        decoratedarklight: "#514f52",
+      },
+    },
   },
   plugins: [],
 };
