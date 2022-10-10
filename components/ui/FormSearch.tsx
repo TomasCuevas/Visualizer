8a6af39c8455216c8e1468cdd5833a1edcb95212
Tomas Cuevas <memo_mister398@hotmail.com>
2022-10-10 13:03:42 -0300

    refactor(Root): update searcher styles and functionality

--- components/ui/FormSearch.tsx
+++ components/ui/FormSearch.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useState, FormEvent } from "react";
+import { useState, FormEvent, useEffect } from "react";
 
 //* icons *//
 import { BsSearch } from "react-icons/bs";
@@ -11,22 +11,41 @@ export const FormSearch: React.FC = () => {
   const onSearch = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    router.push(`/search?search=${toSearch}`);
+    if (toSearch.length > 1)
+      router.push(`/search?search=${toSearch}`, undefined, { scroll: false });
   };
+
+  useEffect(() => {
+    if (router.pathname === "/search") {
+      const query = router.asPath.split("?")[1].split("=")[1];
+      setToSearch(query ? query : "");
+    }
+  }, [router]);
 
   return (
     <form
-      className="flex h-[40px] w-full items-center gap-3 rounded-full bg-decoratelight px-5"
+      className="flex h-[35px] w-full items-center gap-3 rounded-full bg-decoratelight px-5"
       autoComplete="off"
       onSubmit={onSearch}
     >
-      <BsSearch className="h-5 w-5 text-darklighttext" />
+      <button type="submit">
+        <BsSearch className="h-5 w-5 text-darklighttext" />
+      </button>
       <input
         type="text"
         name="search"
         value={toSearch}
+        placeholder="Busca gratis fotos de alta resolución"
         onChange={({ target }) => setToSearch(target.value)}
-        className="w-full  border-none bg-[#0000] outline-none"
+        className="hidden w-full border-none bg-[#0000] outline-none sm:flex"
+      />
+      <input
+        type="text"
+        name="search"
+        value={toSearch}
+        placeholder="Busca fotos"
+        onChange={({ target }) => setToSearch(target.value)}
+        className="w-full border-none bg-[#0000] outline-none sm:hidden"
       />
     </form>
   );
