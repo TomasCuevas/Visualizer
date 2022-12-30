376b9b79a03343b7aa08f25cf587faa5763de49e
Tomas Cuevas <memo_mister398@hotmail.com>
2022-12-30 12:52:55 -0300

    chore: :fire: remove hook useSlider

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,51 +1,22 @@
 import NextLink from "next/link";
 import { useRouter } from "next/router";
 
-//* icons *//
-import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
-
 //* hooks *//
-import { useGetTopics, useSlider } from "../../hooks";
+import { useGetTopics } from "../../hooks";
 
 export const Navbar: React.FC = () => {
   const { topicsQuery } = useGetTopics();
-  const {
-    distanceToLeft,
-    elementsRef,
-    moveNextElement,
-    movePreviousElement,
-    showLeftArrow,
-    showRightArrow,
-    ulRef,
-  } = useSlider();
 
   const router = useRouter();
   const { topic } = router.query;
 
   return (
     <nav className="w-ful sticky top-[60px] z-30 flex h-[60px] items-center justify-center overflow-hidden bg-white px-4 shadow-md lg:top-[70px]">
-      <div
-        className={
-          showLeftArrow
-            ? "absolute left-0 z-10 ml-4 flex h-full w-[30px] items-center justify-center bg-white/75"
-            : "hidden"
-        }
-      >
-        <MdKeyboardArrowLeft
-          onClick={movePreviousElement}
-          className="cursor-pointer text-2xl text-[#222]"
-        />
-      </div>
-      <div className="relative flex h-full w-full items-center justify-center overflow-hidden">
-        <ul
-          ref={ulRef}
-          style={{ left: distanceToLeft }}
-          className="absolute flex h-full items-center gap-5 transition-all duration-300"
-        >
+      <div className="flex h-full w-full items-center overflow-auto">
+        <ul className="flex h-full items-center gap-5 transition-all duration-300">
           {!topicsQuery.isFetching || !topicsQuery.isLoading
-            ? topicsQuery.data?.map(({ title, slug, id }, index) => (
+            ? topicsQuery.data?.map(({ title, slug, id }) => (
                 <li
-                  ref={(element) => (elementsRef.current[index] = element)}
                   key={id}
                   className={
                     slug === topic
@@ -68,18 +39,6 @@ export const Navbar: React.FC = () => {
               ))
             : null}
         </ul>
-      </div>
-      <div
-        className={
-          showRightArrow
-            ? "absolute right-0 z-10 mr-4 flex h-full w-[30px] items-center justify-center bg-white/75"
-            : "hidden"
-        }
-      >
-        <MdKeyboardArrowRight
-          onClick={moveNextElement}
-          className="cursor-pointer text-2xl text-[#222]"
-        />
       </div>
     </nav>
   );
--- hooks/index.ts
+++ hooks/index.ts
@@ -2,4 +2,3 @@ export * from "./useCalcColumns";
 export * from "./useFetchPhotos";
 export * from "./useFetchSearchPhotos";
 export * from "./useGetTopics";
-export * from "./useSlider";
