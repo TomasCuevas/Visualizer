d3e9820ee7b0bfb8b908dd9aa5082d1fecbfd807
Tomas Cuevas <memo_mister398@hotmail.com>
2023-02-05 12:29:41 -0300

    feat: :sparkles: remove _document and update _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 //* stlyes *//
 import "../styles/globals.css";
+import Head from "next/head";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,16 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
+      <Head>
+        <meta
+          name="og:image"
+          content="https://user-images.githubusercontent.com/79057608/216825693-6dd15887-a4d2-4388-8dc9-27b9a7591881.png"
+        />
+        <meta name="og:title" content="Visualizer" />
+        <meta name="og:image:width" content="1920" />
+        <meta name="og:image:height" content="1080" />
+        <link rel="icon" type="image/x-icon" href="/visualizer.svg" />
+      </Head>
       <Component {...pageProps} />
     </QueryClientProvider>
   );
