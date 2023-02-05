e3231b3c7eeccd96ef6958574ef30d020565e371
Tomas Cuevas <memo_mister398@hotmail.com>
2023-02-05 14:19:09 -0300

    style: :bento: update og image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 //* stlyes *//
 import "../styles/globals.css";
-import Head from "next/head";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta
           property="og:image"
-          content="https://user-images.githubusercontent.com/79057608/216825693-6dd15887-a4d2-4388-8dc9-27b9a7591881.png"
+          content="https://user-images.githubusercontent.com/79057608/216833842-78cbc934-8ee7-4b7c-b03e-c4f32e56eceb.png"
         />
         <meta property="og:title" content="Visualizer" />
         <meta property="og:image:width" content="1920" />
