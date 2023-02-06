b4b6b9c0936a0f99c597e94ae2a7eb2dff2ee2ff
Tomas Cuevas <memo_mister398@hotmail.com>
2023-02-06 14:07:04 -0300

    feat: :sparkles: update metatag on _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,12 +14,37 @@ function MyApp({ Component, pageProps }: AppProps) {
       <ReactQueryDevtools initialIsOpen={false} />
       <Head>
         <meta
+          property="og:url"
+          content="https://visualizer-azure.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Visualizer" />
+        <meta
           property="og:image"
           content="https://user-images.githubusercontent.com/79057608/216833842-78cbc934-8ee7-4b7c-b03e-c4f32e56eceb.png"
         />
-        <meta property="og:title" content="Visualizer" />
-        <meta property="og:image:width" content="1920" />
-        <meta property="og:image:height" content="1080" />
+        <meta
+          property="og:image:secure_url"
+          content="https://user-images.githubusercontent.com/79057608/216833842-78cbc934-8ee7-4b7c-b03e-c4f32e56eceb.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="visualizer-azure.vercel.app" />
+        <meta
+          property="twitter:url"
+          content="https://visualizer-azure.vercel.app/"
+        />
+        <meta name="twitter:title" content="Visualizer" />
+        <meta
+          name="twitter:description"
+          content="Pagina principal de visualizer"
+        />
+        <meta
+          name="twitter:image"
+          content="https://user-images.githubusercontent.com/79057608/216833842-78cbc934-8ee7-4b7c-b03e-c4f32e56eceb.png"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <link rel="icon" type="image/x-icon" href="/visualizer.svg" />
       </Head>
       <Component {...pageProps} />
