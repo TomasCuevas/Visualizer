d32cba51eee2f2b36c159f7e793a3d337b234523
Tomas Cuevas <tomas.contact.dev@gmail.com>
2025-03-11 14:02:35 -0300

    refactor: remove unused useResponsiveColumns hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,3 @@
 export * from "./useFetchPhotos/useFetchPhotos";
 export * from "./useFetchSearchPhotos/useFetchSearchPhotos";
 export * from "./useGetTopics/useGetTopics";
-export * from "./useResponsiveColumns/useResponsiveColumns";
